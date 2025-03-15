5523893738c31ae33c7c8133d032e46b579609a2
sakib-xrz <sakibxrz21@gmail.com>
2025-03-16 00:13:43 +0600

    chore: remove unused files and add dist to .gitignore

--- dist/app/modules/Order/order.constant.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.PAYMENT_METHOD = void 0;
-exports.PAYMENT_METHOD = {
-    online_payment: 'online_payment',
-    cash_on_delivery: 'cash_on_delivery',
-};
